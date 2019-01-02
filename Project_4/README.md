7e79c4973938cc2dba3c11af52fefd37d7c56de1
rakochar <rakochar@cs.stonybrook.edu>
2019-01-02 01:40:46 -0800

    Create project 4 and change timeout.

---
+++ Project_4/README.md
@@ -0,0 +1,39 @@
+Node.js framework used: Hapi.js
+
+### Endpoints configured:
+* GET /block/{blockHeight}: Get the block at the given block height.
+* POST /requestValidation : Validates request sent by user.
+* POST /message-signature/validate : Validates signature with wallet address.
+* POST /block : User sends request with star body.
+*
+
+* Steps to run:
+1. npm start
+
+* Steps to register a new star:
+1. Send validation request:
+curl -X POST   http://localhost:8000/requestValidation   -H 'Content-Type: application/json'   -H 'cache-control: no-cache'   -d '{
+    "address":"19YScRyao293zasT54ehvfzdSpJjXCpqha"
+}'
+
+2. Get the signature from the wallet.
+
+3. Validate with wallet address and signature:
+curl -X POST   http://localhost:8000/message-signature/validate   -H 'Content-Type: application/json'   -H 'cache-control: no-cache'   -d '{
+"address":"19YScRyao293zasT54ehvfzdSpJjXCpqha",
+ "signature":"IHd3jvTeA/PBYhaHhLl0wWjzFYbYxEaxNv25R85SyNgWAy09JUm8oTB8SnEsWv7eVPqAgb0DJfNjnWUTfOqItpQ="
+}'
+
+4. Send star body to be stored on the blockchain:
+curl -X POST http://localhost:8000/block -H 'Content-Type: application/json' -H 'cache-control: no-cache' -d '{"body":{
+  "address": "19YScRyao293zasT54ehvfzdSpJjXCpqha",
+  "star": {
+    "dec": "68° 52 56.9",
+    "ra": "16h 29m 1.0s",
+    "story": "Found star using https://www.google.com/sky/"
+    }
+}}'
+
+* Steps to GET a block:
+1. Query with blockHeight:
+curl -G http://localhost:8000/block/33
---
+++ Project_4/server.js
@@ -0,0 +1,158 @@
+'use strict';
+
+const Hapi=require('hapi');
+const Joi=require('joi');
+const simpleChain = require('./simpleChain');
+const mempool = require('./mempool');
+const hex2ascii = require('hex2ascii');
+// Create a server with a host and port
+const server=Hapi.server({
+    host:'127.0.0.1',
+    port:8000
+});
+
+// Add the route
+server.route({
+    method:'GET',
+    path:'/hello',
+    handler:function(request,h) {
+
+        return h.view('index');
+    }
+});
+
+/*
+ * Route for getting a block given the blockheight.
+ */
+server.route({
+   method:'GET',
+   path:'/block/{blockHeight}',
+   handler:async(request,h) => {
+     console.log('GET');
+     try {
+       const parsedBlock = await server.blockchain.getParsedBlock(request.params.blockHeight);
+       return h.response(JSON.parse(parsedBlock)).code(200);
+     } catch (error) {
+       return h.response(error + ' is a block height out of bounds.\n').code(404);
+     }
+   }
+});
+
+/*
+ * Route for POSTing a request for validation to be stored in mempool.
+ */
+server.route({
+    method:['PUT','POST'],
+    path:'/requestValidation',
+    handler:async(request,h) => {
+      console.log('POST');
+      var address = request.payload.address;
+      try {
+        var response = await server.mempool.addToTimeoutRequests(address);
+        console.log('Got response: ' + response.validationWindow);
+        return h.response(response).code(200);
+      } catch (error) {
+        console.log(error + '\n');
+        return h.response('Custom Error\n').code(500);
+      }
+    }
+});
+
+/*
+ * Route for POSTing a signature and address for validation.
+ */
+server.route({
+    method:['PUT','POST'],
+    path:'/message-signature/validate',
+    handler:async(request,h) => {
+      console.log('POST');
+      var address = request.payload.address;
+      var signature = request.payload.signature;
+      try {
+        var response = await server.mempool.validateRequestByWallet(address, signature);
+        // console.log('Got response: ' + response.validationWindow);
+        return h.response(response).code(200);
+      } catch (error) {
+        console.log(error + '\n');
+        return h.response('Request is not valid\n').code(500);
+      }
+    }
+});
+
+/*
+ * Route for registering a star with custom data.
+ */
+server.route({
+    method:['PUT','POST'],
+    path:'/block',
+    handler:async(request,h) => {
+      console.log('POST');
+      var starData = request.payload.body;
+      var address = request.payload.body.address;
+      console.log(request.payload.body);
+      /* Verify if request exists and is valid */
+      var isRequestValid = await server.mempool.verifyValidatedRequest(address);
+      if (!isRequestValid) {
+        return h.response('Previously sent request is not valid!').code(400);
+      }
+      var isStarDataValid = await server.mempool.isStarDataValid(starData);
+      if (!isStarDataValid) {
+        return h.response('Star data is not valid!').code(400);
+      }
+
+      /* Encode story in star data */
+      var encodedStarData = {
+        "address" : starData["address"],
+        "star" : {
+          "ra" : starData["star"]["ra"],
+          "dec" : starData["star"]["dec"],
+          "story" : Buffer(starData["star"]["story"]).toString('hex')
+        }
+      }
+      /* Create new block and add it to block chain */
+      var newBlock = new simpleChain.Block(encodedStarData);
+      var addedBlock = await server.blockchain.addBlock(newBlock)
+                          .catch(error => {
+                            return h.response('error').code(300);
+                          });
+      var addedBlockJSON = JSON.parse(addedBlock);
+
+      // Decoding the encoded star story
+      var encodedStarStory = addedBlockJSON['body']['star']['story'];
+      var decodedStarStory = hex2ascii(encodedStarStory);
+      addedBlockJSON['body']['star']['storyDecoded'] = decodedStarStory;
+
+      var retVal = JSON.stringify(addedBlockJSON).toString();
+
+      server.mempool.removeFromMempoolValid(address);
+      return h.response(retVal).code(200);
+
+    }
+});
+
+// Start the server
+async function start() {
+
+    try {
+        await server.start();
+        server.blockchain = new simpleChain.Blockchain();
+        server.mempool = new mempool.Mempool();
+        await server.register(require('vision'));
+
+        server.views({
+            engines: {
+                html: require('handlebars')
+            },
+            relativeTo: __dirname,
+            path: 'templates'
+        });
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
